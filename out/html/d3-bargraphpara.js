401795fd313660556f47a9f5651170be40cd87b2
Overcraft90 <sloganreader@gmail.com>
2026-04-11 16:17:39 -0600

    rak

---
+++ out/html/d3-bargraphpara.js
@@ -0,0 +1,16 @@
+// Sourced from (Thanks?): https://d3-graph-gallery.com/graph/barplot_stacked_basicWide.html
+
+/* 
+// set the dimensions and margins of the graph
+const margin = {top: 10, right: 30, bottom: 20, left: 50},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+const svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+) */

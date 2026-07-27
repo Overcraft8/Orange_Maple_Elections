35b995db9f41507cef0fc9ea6229631e028d0b66
Overcraft16 <notreal@gmail.com>
2026-07-26 19:35:16 -0600

    let's see

--- out/html/game.js
+++ out/html/game.js
@@ -555,12 +555,12 @@ window.onDisplayContent = function() {
   };
 
 
-  window.justLoaded = true;
-  window.statusTab = "status";
-  window.statusTabRight = "status_right";
-  window.dendryModifyUI = main;
-  console.log("Modifying stats: see dendryUI.dendryEngine.state.qualities");
-
+  // window.justLoaded = true;
+  // window.statusTab = "status";
+  // window.statusTabRight = "status_right";
+  // window.dendryModifyUI = main;
+  // console.log("Modifying stats: see dendryUI.dendryEngine.state.qualities");
+// 
   window.onload = function() {
     // Was originally at false
     window.dendryUI.loadSettings({show_portraits: true});

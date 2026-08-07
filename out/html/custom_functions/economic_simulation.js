98feec301e7a2488cb3d1a5810b363ef6ad63cb6
Overcraft16 <notreal@gmail.com>
2026-08-07 11:06:17 -0600

    try this as well

--- out/html/custom_functions/economic_simulation.js
+++ out/html/custom_functions/economic_simulation.js
@@ -193,7 +193,7 @@ window.region_info_display = function(region_id) {
                     border: ridge; 
                     border-color: #9c8c64;
                 ">
-                    <div style="background-color: rgba(0, 0, 0, 5)">
+                    <div style="background-color: rgba(0, 0, 0, 0.5)">
                         <span class="h1" style="font-size: 10px;">${displayQuantity}${project_name}</span>
                         <span class="horizontal_line"></span>
                         <span style="font-size: 10px;">${project_owner}</span>

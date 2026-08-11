3d2f427ffec3ffc041d9f7df24217cf9278658f8
Overcraft16 <notreal@gmail.com>
2026-08-11 13:50:09 -0600

    a

--- out/html/custom_functions/economic_simulation.js
+++ out/html/custom_functions/economic_simulation.js
@@ -103,7 +103,8 @@ window.economy_presets = {
 window.economy_images = {
     'Manufacturing' : 'robin_hood_mill.jpg',
     'Mining' : 'coal_mining_.jpg', 
-    'Agriculture' : 'sask_farming.jpg', 
+    'Farming' : 'sask_farming.jpg', 
+    'Agricultural Marketing' : 'grain_elevator.jpg',
     'Finance' : 'bank.png'
 };
 
@@ -218,11 +219,7 @@ window.region_info_display = function(region_id) {
                             <span style="font-size: 11px; display: block; margin-top: 4px; margin-bottom: 8px; color: #dcdcdc;">
                                 Priv: ${privateShare}% | Coop: ${coopShare}% | State: ${stateShare}%
                             </span>
-                            <figure title="${tooltipText}" style="
-                                width: 40px; 
-                                height: 40px; 
-                                border-radius: 50%; 
-                                margin: 4px 0 0 0;
+                            <figure class="pie-chart" tooltip="${tooltipText}" style="
                                 background: conic-gradient(
                                     rgba(90, 90, 209, 0.9) 0% ${stop1}%, 
                                     rgba(218, 222, 103, 0.9) ${stop1}% ${stop2}%, 

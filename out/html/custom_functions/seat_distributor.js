6ee5acb38688bd6a0389401aa20ad8ef9e3292c1
Overcraft16 <notreal@gmail.com>
2026-08-17 15:58:34 -0600

    doesn't matter

---
+++ out/html/custom_functions/seat_distributor.js
@@ -0,0 +1,9 @@
+
+// Change seats setup : 
+// [party_region] -> [ccf_ss_]
+
+
+window.change_seats = function([party_region], [seat_changes]) {
+    
+    var Q = window.dendryUI?.dendryEngine?.state?.qualities;
+}; 
